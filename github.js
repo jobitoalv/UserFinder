8c708a8071752e821fb52abf0e516d005a8e9c89
Job Alvarez <jobitoalv@gmail.com>
2022-12-08 16:56:47 +1000

    it will only show one repo at a time

--- github.js
+++ github.js
@@ -2,13 +2,16 @@ class Github {
     constructor() {
       this.client_id = 'd2fcf10b936690a8c741';
       this.client_secret = 'd35ee4b939ec75b0ca0949189803ac3fe8f8352c';
+      this.repos_count = 5;
+      this.repos_sort = 'created: asc';
     }
   
     async getUser(user) {
       const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-  
+      const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
       const profile = await profileResponse.json();
-  
+      const repos = await repoResponse.json();
       return {
         profile
       }
